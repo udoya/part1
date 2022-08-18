c66f9d58ce58f2bb18a7e1524b9c5fb5d7120370
udoya <yadosmith@gmail.com>
2022-08-18 13:51:53 +0900

    mapメソッドを用いたApp.js内のContent関数の修正

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TodoList from "./Todolist";
 
-function App() {
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name} ! You are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const Header = (props) => {
+  console.log(props);
+  const { course } = props;
+  return <h1>{course.name}</h1>;
+};
+
+const Content = (props) => {
+  console.log(props);
+  const { course } = props;
+
+  return (
+    <div>
+      {course.parts.map((part) => {
+        return (
+          <div key={part.name}>
+            <p>{part.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const App = () => {
+  console.log("hello world");
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+      },
+    ],
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +66,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Header course={course} />
+        <Content course={course} />
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,9 +77,15 @@ function App() {
         >
           Learn React
         </a>
+
+        <TodoList />
+        <input type="text" />
+        <button>asahi</button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
+
+//test
